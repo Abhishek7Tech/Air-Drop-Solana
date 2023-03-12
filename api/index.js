053aabd2d59f1a762bcf86b408352c97f69ef2cf
Abhishek7tech <abhishektab45@gmail.com>
2023-03-12 10:48:45 +0530

    added proxy middleware

--- api/index.js
+++ api/index.js
@@ -1,17 +1,27 @@
 const { transferSOLTokens } = require("./account.middlewares");
 
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
-
+const { createProxyMiddleware } = require("http-proxy-middleware");
 // console.log("KEY",dotenv, process.env.REACT_APP_PORT);
 
 const PORT = 8080 || 3001;
 
 const app = express();
-app.use(cors({
-  origin: "https://air-drop-sol.netlify.app/"
-}));
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://air-drop-sol-api.onrender.com",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "",
+    },
+    onProxyRes: (proxyRes, req, res) => {
+      proxyRes.headers["Access-Control-Allow-Origin"] =
+        "https://air-drop-sol.netlify.app/";
+    },
+  })
+);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../", "build")));
